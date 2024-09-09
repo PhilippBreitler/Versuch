f81ad5f3e3111601e0798fd8b62cacd75825926f
Philipp Breitler <philipp.breitler@hotmail.com>
2024-09-09 14:47:47 +0200

    Zweiter Commit!

--- src/Main.java
+++ src/Main.java
@@ -8,6 +8,8 @@ public class Main {
 
         System.out.printf("Hallo und Herzlich Willkommen");
 
+        System.out.printf("Hallo und Herzlich Willkommen");
+
         for (int i = 1; i <= 5; i++) {
             //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
             // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
